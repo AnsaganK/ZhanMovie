fda097199bd44dfc21c3c7e080058656093cd32a
Ansagan-Kabdolla <kabdolla00.00@mail.ru>
2021-05-02 00:14:01 +0600

    add static for pythonanywhere

--- kinoteatr/settings.py
+++ kinoteatr/settings.py
@@ -135,8 +135,8 @@ LOGOUT_REDIRECT_URL = "index"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+#STATIC_URL = '/static/'
+#STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
