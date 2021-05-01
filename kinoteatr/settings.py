ae4980b588a3911ffef096657558801602b2b68c
Ansagan-Kabdolla <kabdolla00.00@mail.ru>
2021-05-02 04:52:08 +0600

    add static for pythonanywhere

--- kinoteatr/settings.py
+++ kinoteatr/settings.py
@@ -138,7 +138,8 @@ LOGOUT_REDIRECT_URL = "index"
 STATIC_URL = '/static/'
 #STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = Path('static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
