482b0191d164931287fe803698304c23446a347e
Ansagan-Kabdolla <kabdolla00.00@mail.ru>
2021-05-04 04:09:36 +0600

    add category movies

--- movie/url_api.py
+++ movie/url_api.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
 from movie.views import MovieListApi, MovieDetailApi, MovieRandomDetailApi, CategoryListApi, GenreListApi, \
-    CategoryMovieListApi
+    CategoryMovieListApi, MovieSearchApi
 
 urlpatterns = [
     url(r'^movies/$', MovieListApi.as_view()),
+    url(r'^movies/search$', MovieSearchApi.as_view()),
     url(r'^movies/(?P<pk>[0-9]+)/$', MovieDetailApi.as_view()),
     url(r'^movies/random$', MovieRandomDetailApi.as_view()),
     url(r'^categories/(?P<pk>[0-9]+)/movies/$', CategoryMovieListApi.as_view()),
