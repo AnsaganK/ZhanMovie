ce5141190e6279e03fb918198376ee4044e889f5
Ansagan-Kabdolla <kabdolla00.00@mail.ru>
2021-06-03 21:48:19 +0600

    add button telegram

--- kinoteatr/urls.py
+++ kinoteatr/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 #from movie.urls import router
 
+
 urlpatterns = [
     url(r'^api/', include('movie.url_api')),
     url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
